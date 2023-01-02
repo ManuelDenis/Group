22934bfcf7439e28dc1bbcc00a52563fa88bc84b
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-01-02 12:38:22 +0200

    group added 2

--- drink.py
+++
@@ -1,7 +0,0 @@
-from project.product import Product
-
-
-class Drink(Product):
-    def __init__(self, name: str, quantity: int = 10):
-        super().__init__(name, quantity)
-        self.quantity = quantity
